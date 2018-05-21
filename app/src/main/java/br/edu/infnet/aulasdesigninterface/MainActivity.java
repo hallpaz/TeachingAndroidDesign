d7509341505eeaeb92fa90e1e878b5c261ff56d8
hallpaz <hallpaaz@gmail.com>
2018-05-21 17:33:04 -0300

    after have finished the birthdayCard project for class presentation

---
+++ app/src/main/java/br/edu/infnet/aulasdesigninterface/MainActivity.java
@@ -0,0 +1,25 @@
+package br.edu.infnet.aulasdesigninterface;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void birthDayCardLL(View v){
+        Intent intent = new Intent(this, BirthdayCardLL.class);
+        startActivity(intent);
+    }
+
+    public void birthDayCardRL(View v){
+        Intent intent = new Intent(this, BirthdayCardRL.class);
+        startActivity(intent);
+    }
+}
